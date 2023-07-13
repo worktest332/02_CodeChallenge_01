b174428b0ee6c7bbd542bb915dcd66486496a2d1
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-07-13 12:48:34 +0200

    Add files via upload

---
+++ challenge_01.js
@@ -0,0 +1,11 @@
+
+/*********   CODE CHALLENGE 01 **********/
+ 
+/*
+Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+Berechnen Sie die Summe der beiden Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+"Die Summe der Zahlen ist: summe"
+*/
+
